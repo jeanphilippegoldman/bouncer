ebe27601c2efb66602a4c65518d43173d57cafc2
Jean-Philippe Goldman <jeanphilippegoldman@gmail.com>
2016-04-02 15:55:07 +0200

    add size management

---
+++ mySize.js
@@ -0,0 +1,21 @@
+//from http://www.howtocreate.co.uk/tutorials/javascript/browserwindow
+
+function mySize() {
+  var myWidth = 0, myHeight = 0;
+  if( typeof( window.innerWidth ) == 'number' ) {
+    //Non-IE
+    myWidth = window.innerWidth;
+    myHeight = window.innerHeight;
+  } else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
+    //IE 6+ in 'standards compliant mode'
+    myWidth = document.documentElement.clientWidth;
+    myHeight = document.documentElement.clientHeight;
+  } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+    //IE 4 compatible
+    myWidth = document.body.clientWidth;
+    myHeight = document.body.clientHeight;
+  }
+  //window.alert( 'Width = ' + myWidth );
+  //window.alert( 'Height = ' + myHeight );
+  return [ myWidth, myHeight ];
+}
